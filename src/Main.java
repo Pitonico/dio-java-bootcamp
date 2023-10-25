12fa7a6caa13f4e92db0bf9a5466b02fee6bfe60
Pitonico <diogba@gmail.com>
2023-10-25 00:27:34 -0300

    Alterando as classes Main e Dev.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,58 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Curso cursoJava = new Curso();
-        cursoJava.setTitulo("Java");
-        cursoJava.setDescricao("Curso Java");
-        cursoJava.setCargaHoraria(20);
-        double xp = cursoJava.calcularXP();
-        System.out.println(cursoJava);
-        System.out.println("XP: " + xp);
+        Curso cursoJavaIniciante = new Curso();
+        cursoJavaIniciante.setTitulo("Curso Java Iniciante");
+        cursoJavaIniciante.setDescricao("Descricao do Curso Java Iniciate ");
+        cursoJavaIniciante.setCargaHoraria(20);
+
+        Curso cursoJavaIntermediario = new Curso();
+        cursoJavaIntermediario.setTitulo("Curso Java Intermediário");
+        cursoJavaIntermediario.setDescricao("Descricao do Curso Java Intermediário");
+        cursoJavaIntermediario.setCargaHoraria(30);
+
+        Curso cursoJavaAvancado = new Curso();
+        cursoJavaAvancado.setTitulo("Curso Java Avancado");
+        cursoJavaAvancado.setDescricao("Descrição do Curso Java Avançado");
+        cursoJavaAvancado.setCargaHoraria(35);
+
+        Mentoria mentoriaJava = new Mentoria();
+        mentoriaJava.setTitulo("Mentoria Java");
+        mentoriaJava.setDescricao("Descrição da Mentoria Java");
+        mentoriaJava.setData(LocalDate.now());
+
+        Bootcamp bootcampJava = new Bootcamp();
+        bootcampJava.setNome("Bootcamp Java");
+        bootcampJava.setDescricao("Descrição do Bootcamp Java");
+        bootcampJava.getConteudos().add(cursoJavaIniciante);
+        bootcampJava.getConteudos().add(cursoJavaIntermediario);
+        bootcampJava.getConteudos().add(cursoJavaAvancado);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("João");
+        dev1.inscreverBootCamp(bootcampJava);
+        System.out.println(dev1.getNome() + " Conteudos Inscritos: " + dev1.getConteudoInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println(dev1.getNome() + " Conteudos Concluídos: " + dev1.getConteudoConcluidos());
+        System.out.println(dev1.getNome() + " Conteudos Inscritos: " + dev1.getConteudoInscritos());
+        System.out.println(dev1.getNome() + " XP: " + dev1.calcularTotalXP());
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Renata");
+        dev2.inscreverBootCamp(bootcampJava);
+        System.out.println(dev2.getNome() + " Conteudos Inscritos: " + dev2.getConteudoInscritos());
+        dev2.progredir();
+        dev2.progredir();
+        System.out.println(dev2.getNome() + " Conteudos Concluídos: " + dev2.getConteudoConcluidos());
+        System.out.println(dev1.getNome() + " Conteudos Inscritos: " + dev1.getConteudoInscritos());
+        System.out.println(dev2.getNome() + " XP: " + dev2.calcularTotalXP());
     }
 }
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,10 +10,26 @@ public class Dev {
     private Set<Conteudo> conteudoInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootCamp(Bootcamp bootcamp) {}
+    public void inscreverBootCamp(Bootcamp bootcamp) {
+        this.conteudoInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void progredir(){}
-    public void calcularTotalXP(){}
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudoInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudoInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não esta matriculado em nenhum conteudo");
+        }
+    }
+    public double calcularTotalXP(){
+        return this.conteudoConcluidos
+                .stream()
+                .mapToDouble(Conteudo::calcularXP)
+                .sum();
+    }
 
     public String getNome() {
         return nome;
