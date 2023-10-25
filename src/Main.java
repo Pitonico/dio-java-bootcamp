a179ecbf87d95c82374b29b20304121a34069c31
Pitonico <diogba@gmail.com>
2023-10-24 23:19:58 -0300

    Criando as classes Conteudo, Bootcamp e Dev. Alterando as classes Curso e Mentoria.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("Java");
+        cursoJava.setDescricao("Curso Java");
+        cursoJava.setCargaHoraria(20);
+        double xp = cursoJava.calcularXP();
+        System.out.println(cursoJava);
+        System.out.println("XP: " + xp);
     }
 }
